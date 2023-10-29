4c35e7b50d59567a5672c980b1bb2d3f4c2527ab
Jake Armstrong <15316168+rhit-armstrjj@users.noreply.github.com>
2023-10-29 16:24:29 -0400

    Setup Alias + Project Deps

---
+++ lib/OSMI-System/OSMI.h
@@ -0,0 +1,12 @@
+#ifndef _OSMI_H_
+#define _OSMI_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RTC_TIME() millis()
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,54 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/*
+  Ellipse drawing example
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  This sketch does not use any fonts.
+*/
+#include <SPI.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+
+void setup(void) {
+  tft.init();
+
+  tft.setRotation(1);
+  Serial.begin(115200);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  tft.fillScreen(TFT_BLACK);
+
+  // Draw some random circles
+  for (int i = 0; i < 40; i++)
+  {
+    int rx = random(60);
+    int ry = random(60);
+    int x = rx + random(480 - rx - rx);
+    int y = ry + random(320 - ry - ry);
+    tft.fillEllipse(x, y, rx, ry, random(0xFFFF));
+  }
+  Serial.println("Fuck You");
+
+  delay(2000);
+  tft.fillScreen(TFT_BLACK);
+
+  for (int i = 0; i < 40; i++)
+  {
+    int rx = random(60);
+    int ry = random(60);
+    int x = rx + random(480 - rx - rx);
+    int y = ry + random(320 - ry - ry);
+    tft.drawEllipse(x, y, rx, ry, random(0xFFFF));
+  }
+
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+
