f560773c4d7408190edb20fc53dee5c208c99994
rhit-GabeNeise <neisega@rose-hulman.edu>
2023-11-10 22:22:46 -0500

    added todo for state message handling

--- lib/OSMI-Control/OSMI-Control.cpp
+++ lib/OSMI-Control/OSMI-Control.cpp
@@ -24,17 +24,20 @@ void LoopControl(void *params)
     float R1 = 10000; // known resistance
     float R = 0;      // unknown resistance
     float buffer = 0;
-    
+
     // position
     positRaw = analogRead(A0); // reads value of the potentiometer
     Serial.print("position: ");
     Serial.println(positRaw);
-    
-    //Find our new delay time from our setpoint.
+
+    // Find our new delay time from our setpoint.
     Serial.print("setpoint: ");
     Serial.println(positRaw);
     uint16_t new_delay = state->pidChannels[0].step(state->targetPosition[0], positRaw);
 
-    //
-  
+    if (state->activeChannels[0])
+    {
+        // TODO: set Wei's driver to new delay for current channel
+        // kp < 1
+    }
 }
