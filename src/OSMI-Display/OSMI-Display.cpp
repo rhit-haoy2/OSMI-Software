3d3cff580fa9b10db02117f94fe27b40313f5c2a
huangw4 <73304411+huangw4@users.noreply.github.com>
2023-12-17 16:26:54 -0500

    LVGL Test

--- src/OSMI-Display/OSMI-Display.cpp
+++ src/OSMI-Display/OSMI-Display.cpp
@@ -29,6 +29,30 @@ void display_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
     lv_disp_flush_ready(disp);
 }
 
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+static void slider_event_cb(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target(e);
+
+    lv_obj_t * label = lv_obj_get_child(slider, 0);
+    /*Refresh the text*/
+    lv_label_set_text_fmt(label, "%" LV_PRId32, lv_slider_get_value(slider));
+    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+}
+
 void DisplayTask(void *params)
 {
 
@@ -48,6 +72,28 @@ void DisplayTask(void *params)
     lv_obj_t *label = lv_label_create( lv_scr_act() );
     lv_label_set_text( label, "Hello Ardino and LVGL!");
     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * btnlabel = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(btnlabel, "Button");                     /*Set the labels text*/
+    lv_obj_center(btnlabel);
+
+
+    /*Create a slider in the center of the display*/
+    lv_obj_t * slider = lv_slider_create(lv_scr_act());
+    lv_obj_set_width(slider, 200);                          /*Set the width*/
+    lv_obj_center(slider);                                  /*Align to the center of the parent (screen)*/
+    lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
+
+    /*Create a label above the slider*/
+    lv_obj_t *sliderlabel = lv_label_create(slider);
+    lv_label_set_text(sliderlabel, "0");
+    lv_obj_align_to(sliderlabel, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+
 
     QueueHandle_t *handle = (QueueHandle_t *)params;
     while (true)
